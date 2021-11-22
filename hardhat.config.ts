0f73f063910013ade86e506e4e3d6d686a783ea5
@raywom <40425058+raywom@users.noreply.github.com>
2021-11-22 21:57:41 +0100

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,8 +9,6 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -19,16 +17,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
--- test/test.ts
+++ test/test.ts
@@ -2,16 +2,15 @@ import { expect } from "chai";
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
-describe("Test", function () {
-  it("Сrediting to the balance", async function () {
+describe("Unit tests", function () {
+  it("Deploying", async function () {
     const Charity = await ethers.getContractFactory("Charity");
     const charity = await Charity.deploy();
     await charity.deployed();
     const [accountOne, accountTwo] = await ethers.getSigners();
-
   });
 
-  it("Withdrawal of ETH from the contract", async function () {
+  it("Charity withdraw from address", async function () {
     const Charity = await ethers.getContractFactory("Charity");
     const charity = await Charity.deploy();
     await charity.deployed();
@@ -25,15 +24,15 @@ describe("Test", function () {
       to: charity.address,
       value: ethers.utils.parseEther("4"),
     });
-
-    await expect(
-      charity.connect(accountTwo).withdraw(await accountThree.getAddress())
-    ).to.be.revertedWith("ERROR: You are not the owner.");
+    await accountTwo.sendTransaction({
+      to: charity.address,
+      value: ethers.utils.parseEther("8"),
+    });
 
     await charity.withdraw(await accountThree.getAddress());
   });
 
-  it("List of users", async function () {
+  it("Check contributors", async function () {
     const Charity = await ethers.getContractFactory("Charity");
     const charity = await Charity.deploy();
     await charity.deployed();
@@ -42,10 +41,6 @@ describe("Test", function () {
     await accountOne.sendTransaction({
       to: charity.address,
       value: ethers.utils.parseEther("1"),
-    });
-    await accountTwo.sendTransaction({
-      to: charity.address,
-      value: ethers.utils.parseEther("4"),
     });
     await accountTwo.sendTransaction({
       to: charity.address,
